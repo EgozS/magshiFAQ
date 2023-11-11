89b63d105d67b6f88d3d8c68b52a77935e0649bd
skep <75840178+SkepSickomode@users.noreply.github.com>
2023-11-11 21:28:01 +0200

    weeeeeeeeee

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const questions = document.querySelectorAll('.question');
+    const questions = document.querySelectorAll('.question-wrapper');
 
     questions.forEach(function (question) {
         const dropdownTrigger = question.querySelector('.dropdown-trigger');
-        const dropdownCardContent = document.querySelector('.dropdown-card-content');
+        const dropdownCardContent = question.querySelector('.dropdown-card-content');
+        const questionContent = question.querySelector('.question');
 
         dropdownTrigger.addEventListener('click', function (event) {
             event.stopPropagation();
-            question.classList.toggle('clicked');
+            questionContent.classList.toggle('clicked');
             dropdownCardContent.style.display = (dropdownCardContent.style.display === 'block') ? 'none' : 'block';
         });
     });
 
     document.addEventListener('click', function () {
         questions.forEach(function (q) {
-            q.classList.remove('clicked');
-            const dropdownCardContent = document.querySelector('.dropdown-card-content');
+            const questionContent = q.querySelector('.question');
+            questionContent.classList.remove('clicked');
+            const dropdownCardContent = q.querySelector('.dropdown-card-content');
             if (dropdownCardContent) {
                 dropdownCardContent.style.display = 'none';
             }
